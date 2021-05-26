060c57442dcbe0f71ef97dab4ba58e911b41741a
Gabriel Meyer <meyergabriel@live.de>
2021-05-26 19:44:54 +0200

    Slightly Improvements: Image + CSS support

--- webpack.react.js
+++ webpack.react.js
@@ -1,49 +1,53 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-	entry: "./src/renderer.tsx",
-	target: "electron-renderer",
-	devtool: "source-map",
-	devServer: {
-		contentBase: path.join(__dirname, "dist/renderer.js"),
-		compress: true,
-		port: 9000,
-	},
-	resolve: {
-		alias: {
-			["@"]: path.resolve(__dirname, "src"),
-		},
-		extensions: [".tsx", ".ts", ".js"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts(x?)$/,
-				include: /src/,
-				use: [{ loader: "ts-loader" }],
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: ["style-loader", "css-loader", "sass-loader"],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
-	output: {
-		path: __dirname + "/dist",
-		filename: "renderer.js",
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-	],
+  entry: './src/renderer.tsx',
+  target: 'electron-renderer',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist/renderer.js'),
+    compress: true,
+    port: 9000
+  },
+  resolve: {
+    alias: {
+      ['@']: path.resolve(__dirname, 'src')
+    },
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        include: /src/,
+        use: [{ loader: 'ts-loader' }]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'renderer.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
